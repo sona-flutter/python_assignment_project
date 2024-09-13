483c65d76c1f1bbdd592defd9069c936ce372a19
sona-flutter <fluttersonali07@gmail.com>
2024-09-13 14:02:12 +0530

    done

---
+++ pythonfullcourse/List/IterationList/Ex1.py
@@ -0,0 +1,4 @@
+l=[10,20,30,40,50,60]
+t=len(l)
+for n in range(t):
+    print(l[n])
---
+++ pythonfullcourse/List/comprehension/ex1.py
@@ -0,0 +1,7 @@
+l=[]
+for i in range(1,101):
+    l.append(i)
+print(l)
+
+n=[m for m in range(1,101) if m%2==0 ]
+print(n)
